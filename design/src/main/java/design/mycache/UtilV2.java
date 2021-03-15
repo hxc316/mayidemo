cef4b7d20a5604897ae8358eaad535fad81d82d7
Jeremy <jeremy@1yuans.com>
2021-03-15 09:46:12 +0800

    query cache

--- design/src/main/java/design/mycache/UtilV2.java
+++ design/src/main/java/design/mycache/UtilV2.java
@@ -1,11 +1,13 @@
 package design.mycache;
 
+import org.springframework.context.annotation.Configuration;
+
 import java.util.HashMap;
 import java.util.function.Function;
 
 public class UtilV2<T,R> {
 
-    private HashMap<Object,R> cache = new HashMap();
+    private HashMap<T,R> cache = new HashMap();
 
     public  R getInfoWithCache(T id, Function<T,R> consumer){
         R userInfo = cache.get(id);
